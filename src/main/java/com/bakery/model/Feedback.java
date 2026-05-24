89fcae036eb0977724aee7b7a8e72096ad85bcb2
it25103745-blip <it25103745@my.sliit.lk>
2026-05-24 11:52:10 +0530

    [IT25103745] Add Feedback model - Ratings & Documentation module

--- src/main/java/com/bakery/model/Feedback.java
+++ src/main/java/com/bakery/model/Feedback.java
@@ -1,4 +1,8 @@
 package com.bakery.model;
 
 public class Feedback  extends BakeryRecord {
+    private String customerName;
+    private String rating;
+    private String comment;
+    private String moderationStatus;
 }
