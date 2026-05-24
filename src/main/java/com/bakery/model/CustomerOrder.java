1c3bf7133cd735578cc868e8f183d209add38059
IT25103749 <it25103749@my.sliit.lk>
2026-05-24 21:30:03 +0530

    [IT25103749] Add CustomerOrder model - Cart & Order Tracking

--- src/main/java/com/bakery/model/CustomerOrder.java
+++ src/main/java/com/bakery/model/CustomerOrder.java
@@ -1,4 +1,25 @@
 package com.bakery.model;
 
+import java.util.List;
+
 public class CustomerOrder extends BakeryRecord {
+    private String customerName;
+    private String orderedItems;
+    private String totalAmount;
+    private String orderStatus;
+    private String paymentStatus;
+
+    public CustomerOrder(){
+
+    }
+    public CustomerOrder(String id, String customerName, String orderedItems, String totalAmount, String orderStatus, String paymentStatus){
+        super(id);
+        this.customerName = customerName;
+        this.orderedItems = orderedItems;
+        this.totalAmount = totalAmount;
+        this.orderStatus = orderStatus;
+        this.paymentStatus = paymentStatus;
+    }
+
+
 }
