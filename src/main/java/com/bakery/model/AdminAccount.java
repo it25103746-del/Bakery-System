14d13ef16bf3bd129f8d65a77f57afa56faab008
Perera K. V. R. <it25103746@my.sliit.lk>
2026-05-24 20:30:02 +0530

    [IT25103746] Add AdminAccount model - Admin Management module

--- src/main/java/com/bakery/model/AdminAccount.java
+++ src/main/java/com/bakery/model/AdminAccount.java
@@ -6,4 +6,16 @@ public class AdminAccount extends BakeryRecord {
     private String role;
     private String stockResponsibility;
     private String expiryStatus;
+
+    public AdminAccount(){
+    }
+
+    public AdminAccount(String id, String adminName, String email, String role, String stockResponsibility, String expiryStatus) {
+        super(id);
+        this.adminName = adminName;
+        this.email = email;
+        this.role = role;
+        this.stockResponsibility = stockResponsibility;
+        this.expiryStatus = expiryStatus;
+    }
 }
