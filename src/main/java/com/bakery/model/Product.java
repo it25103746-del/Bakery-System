d0b296aab9f3598b2c840cca5a9810e15c61939f
IT25103748 <it25103748@my.sliit.lk>
2026-05-25 15:25:45 +0530

    [IT25103748] Add product model-Product & Category Mangagement

--- src/main/java/com/bakery/model/Product.java
+++ src/main/java/com/bakery/model/Product.java
@@ -3,4 +3,18 @@ package com.bakery.model;
 import java.util.List;
 
 public class Product extends BakeryRecord {
+    private String productName;
+    private String category;
+    private String price;
+    private String details;
+    private  String availability;
+    private String stockAmount;
+
+    public Product(){
+
+    }
+
+    public Product(String id, String productName,String category, String price,String details,String availability){
+        this (id, productName,category,price,details,availability"0");
+    }
 }
