9b6ef91dfc92dc8a80a54819e3ed3bc499dc6f39
Sandeepa Nishamal IT25103747 <it25103747@my.sliit.lk>
2026-05-25 12:52:46 +0530

    [IT25103747] Add UserAccount model - User Authentication module

--- src/main/java/com/bakery/model/UserAccount.java
+++ src/main/java/com/bakery/model/UserAccount.java
@@ -12,3 +12,25 @@ public class UserAccount extends BakeryRecord {
 
     public UserAccount() {
     }
+    public UserAccount(String id, String fullName, String email, String phone, String address, String accountStatus) {
+        this(id, fullName, email, phone, address, accountStatus, "");
+    }
+
+    public UserAccount(String id, String fullName, String email, String phone, String address, String accountStatus, String temporaryPassword) {
+        super(id);
+        this.fullName = fullName;
+        this.email = email;
+        this.phone = phone;
+        this.address = address;
+        this.accountStatus = accountStatus;
+        this.temporaryPassword = temporaryPassword;
+    }
+    @Override
+    public String getDisplayName() {
+        return fullName;
+    }
+
+    @Override
+    public List<String> toFileFields() {
+        return List.of(fullName, email, phone, address, accountStatus, temporaryPassword);
+    }
