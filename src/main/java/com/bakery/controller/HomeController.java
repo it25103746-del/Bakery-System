cd19152bb4fded0795bdc25d2b7c960d8400b950
Perera K. V. R. <it25103746@my.sliit.lk>
2026-05-26 11:30:23 +0530

    Update HomeController.java

--- src/main/java/com/bakery/controller/HomeController.java
+++ src/main/java/com/bakery/controller/HomeController.java
@@ -1,5 +1,6 @@
 package com.bakery.controller;
 
+import com.bakery.service.ModuleCatalog;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
